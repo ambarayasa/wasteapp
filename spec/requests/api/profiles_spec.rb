6a022f412e740ecf782b6de8a8a900637848fd83
Faiz Authar <faizauthar@gmail.com>
2022-07-08 10:03:44 +0700

    swagger: Profile: prettier!

--- spec/requests/api/profiles_spec.rb
+++ spec/requests/api/profiles_spec.rb
@@ -1,45 +1,60 @@
-require 'swagger_helper'
+require "swagger_helper"
 
 RSpec.describe "Profile API", type: :request do
-    
-    path "/profile" do
-        get "Retrieves a profile" do
-            tags "profile"
+  path "/profile" do
+    get "Retrieves a profile" do
+      tags "profile"
 
-            consumes 'application/json'
-            parameter name: :profile, in: :body, schema: {
-                type: :object,
-                properties: {
-                  email: { type: :string },
-                },
-                required: [ 'email' ]
-              }
-
-            produces "application/json"
-            response "200", "Profile found" do
-                schema type: :object,
-                properties: {
-                    data: {
-                        properties: {
-                            name: {type: :string},
-                            username: {type: :string},
-                            email: {type: :string},
-                            address: {type: :string},
-                            balance: {type: :float},
-                            role: {type: :string},
-                        }
+      consumes "application/json"
+      parameter name: :profile,
+                in: :body,
+                schema: {
+                  type: :object,
+                  properties: {
+                    email: {
+                      type: :string
                     }
-                },
-                required: [ "name", "username", "email", "address", "balance", "role"]
+                  },
+                  required: ["email"]
+                }
 
-                let(:profile) { create(:profile) }
-                run_test!
-            end
+      produces "application/json"
+      response "200", "Profile found" do
+        schema type: :object,
+               properties: {
+                 data: {
+                   properties: {
+                     name: {
+                       type: :string
+                     },
+                     username: {
+                       type: :string
+                     },
+                     email: {
+                       type: :string
+                     },
+                     address: {
+                       type: :string
+                     },
+                     balance: {
+                       type: :float
+                     },
+                     role: {
+                       type: :string
+                     }
+                   }
+                 }
+               },
+               required: %w[name username email address balance role]
 
-            response '404', 'Profile not found' do
-                let(:profile) { 'invalid' }
-                run_test!
-              end
-        end
+        let(:profile) { create(:profile) }
+        run_test!
+      end
+
+      response "404", "Profile not found" do
+        let(:profile) { "invalid" }
+        run_test!
+      end
     end
+  end
 end
